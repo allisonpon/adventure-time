4053f343c8750a8d694e99f5508e97101ee94b48
Allison Pon <allisonpon@gmail.com>
2015-07-26 00:11:00 -0600

    Fix sidebar and navbar.

--- header.php
+++ header.php
@@ -23,17 +23,19 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'adventure-time' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-      <?php if ( get_header_image() ) : ?>
-        <img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-      <?php endif; // End header image check. ?>
-      <div class="header-box">
-        <div class="header-text">
-  			  <div class="site-description"><?php bloginfo( 'description' ); ?></div>
-          <div class="site-location">Currently: <?php echo get_theme_mod("current_location"); ?></div>
-  		  </div>
-      </div>
-    </div><!-- .site-branding -->
+		<?php if ( is_front_page() && is_home()) : ?>
+      <div class="site-branding">
+        <?php if ( get_header_image() ) : ?>
+          <img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+          <div class="header-box">
+            <div class="header-text">
+              <div class="site-description"><?php bloginfo( 'description' ); ?></div>
+              <div class="site-location">Currently: <?php echo get_theme_mod("current_location"); ?></div>
+            </div>
+          </div>
+        <?php endif; // End header image check. ?>
+      </div><!-- .site-branding -->
+    <?php endif; // End header check. ?>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'adventure-time' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
