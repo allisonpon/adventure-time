0c7387aeb7efc5baf911126989e7a4bd41d829c3
Allison Pon <allisonpon@gmail.com>
2015-07-25 23:27:33 -0600

    Styles for posts.

--- footer.php
+++ footer.php
@@ -13,9 +13,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'adventure-time' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'adventure-time' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'adventure-time' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'adventure-time' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'adventure-time' ), 'adventure-time', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'adventure-time' ), 'Adventure Time', 'ManicPixieCowgirl' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
